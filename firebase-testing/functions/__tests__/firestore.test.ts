4c3cb7bc7a1e70c4c10a103f2bdbb061cb74475a
hagyyyy1992 <keiichi.hagiwara.1992@gmail.com>
2021-11-25 19:53:41 +0900

    firebase-testing

---
+++ firebase-testing/functions/__tests__/firestore.test.ts
@@ -0,0 +1,35 @@
+import * as firebase from '@firebase/rules-unit-testing'
+import * as fs from 'fs'
+import 'jest'
+
+let testEnv: firebase.RulesTestEnvironment
+
+beforeAll(async () => {
+    testEnv = await firebase.initializeTestEnvironment({
+        projectId: 'demo-test',
+        firestore: { rules: fs.readFileSync("../firestore.rules", "utf8") },
+    })
+})
+
+beforeEach(async () => await testEnv.clearFirestore())
+
+afterAll(async () => await testEnv.cleanup())
+
+describe("firestore-test", () => {
+
+    describe("messageコレクションのテスト", () => {
+        test("messageの読み込みを実行", async () => {
+            const db = testEnv.authenticatedContext("testUser").firestore();
+            const message = db.collection("message").doc("testUser");
+            await firebase.assertSucceeds(message.get());
+        });
+
+        test("messageへ書き込みを実行", async () => {
+            const db = testEnv.authenticatedContext("testUser").firestore();
+            const message = db.collection("message").doc("testUser");
+            await firebase.assertSucceeds(
+                message.set({ text: "test" })
+            );
+        });
+    });
+})
