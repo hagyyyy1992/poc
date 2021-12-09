ecc8fee92ba57bfc69d85119d88a0000e71f62af
hagyyyy1992 <keiichi.hagiwara.1992@gmail.com>
2021-12-09 09:16:10 +0900

    テスト追加

--- firebase-testing/functions/__tests__/firestore.test.ts
+++ firebase-testing/functions/__tests__/firestore.test.ts
@@ -15,14 +15,100 @@ beforeEach(async () => await testEnv.clearFirestore());
 
 afterAll(async () => await testEnv.cleanup());
 
-test("messageの読み込みを実行", async () => {
-    const db = testEnv.authenticatedContext("testUser").firestore();
-    const message = db.collection("message").doc("testUser");
-    await firebase.assertSucceeds(message.get());
+/**
+ * セット出来ること
+ */
+test("SET - Authed", async () => {
+  const db = testEnv.authenticatedContext("testUser").firestore();
+  const message = db.collection("message").doc("testUser");
+  await firebase.assertSucceeds(message.set({text: "test"}));
 });
 
-test("messageへ書き込みを実行", async () => {
-    const db = testEnv.authenticatedContext("testUser").firestore();
-    const message = db.collection("message").doc("testUser");
-    await firebase.assertSucceeds(message.set({ text: "test" }));
+/**
+ * セット出来ないこと
+ */
+test("SET - Not Authed", async () => {
+  const db = testEnv.unauthenticatedContext().firestore();
+  const message = db.collection("message").doc("testUser");
+  await firebase.assertFails(message.set({text: "test"}));
+});
+
+/**
+ * 更新出来ること
+ */
+test("UPDATE - Matched UserID", async () => {
+  const db = testEnv.authenticatedContext("testUser").firestore();
+  const message = db.collection("message").doc("testUser");
+  await message.set({text: "test"});
+  await firebase.assertSucceeds(message.update({text: "test2"}));
+});
+
+/**
+ * 更新出来ないこと
+ */
+test("UPDATE - Unmatched UserID", async () => {
+  // 他者のデータを登録
+  const _db = testEnv.authenticatedContext("testUser2").firestore();
+  const _message = _db.collection("message").doc("testUser2");
+  await _message.set({text: "test"});
+  const db = testEnv.authenticatedContext("testUser").firestore();
+  const message = db.collection("message").doc("testUser2");
+  await firebase.assertFails(message.update({text: "test"}));
+});
+
+/**
+ * 削除出来ないこと
+ */
+test("DELETE - Authed", async () => {
+  const db = testEnv.authenticatedContext("testUser").firestore();
+  const message = db.collection("message").doc("testUser");
+  await firebase.assertFails(message.delete());
+});
+
+/**
+ * 削除出来ないこと
+ */
+test("DELETE - Not Authed", async () => {
+  const _db = testEnv.authenticatedContext("testUser").firestore();
+  const _message = _db.collection("message").doc("testUser");
+  await _message.set({text: "test"});
+  const db = testEnv.unauthenticatedContext().firestore();
+  const message = db.collection("message").doc("testUser");
+  await firebase.assertFails(message.delete());
+});
+
+/**
+ * 1件取得出来ること
+ */
+test("GET - Authed", async () => {
+  const db = testEnv.authenticatedContext("testUser").firestore();
+  const message = db.collection("message").doc("testUser");
+  await firebase.assertSucceeds(message.get());
+});
+
+/**
+ * 1件取得出来ないこと
+ */
+test("GET - Not Authed", async () => {
+  const db = testEnv.unauthenticatedContext().firestore();
+  const message = db.collection("message").doc("testUser");
+  await firebase.assertFails(message.get());
+});
+
+/**
+ * 全件取得出来ること
+ */
+test("LIST - Authed", async () => {
+  const db = testEnv.authenticatedContext("testUser").firestore();
+  const message = db.collection("message").doc("testUser");
+  await firebase.assertSucceeds(message.get());
+});
+
+/**
+ * 全件取得出来ないこと
+ */
+test("LIST - Not Authed", async () => {
+  const db = testEnv.unauthenticatedContext().firestore();
+  const message = db.collection("message");
+  await firebase.assertFails(message.get());
 });
